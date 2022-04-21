b230914ecd528a50b9c4894db548ce5e204985bb
Asmah Emmanuel <atbhae100@gmail.com>
2022-04-21 06:53:14 -0700

    Authentication

---
+++ utils/validation.js
@@ -0,0 +1,18 @@
+const joi = require("joi");
+
+const registerSchema = joi.object({
+    username: joi.string().min(2).max(30).required(),
+    email: joi.string().email().required(),
+    password: joi.string().min(6).required(),
+});
+
+const loginSchema = joi.object({
+    username: joi.string().min(2).max(30).required(),
+    email: joi.string().email().required(),
+    password: joi.string().min(6).required(),
+});
+
+module.exports = {
+    registerSchema,
+    loginSchema,
+}
